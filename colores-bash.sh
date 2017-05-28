0dd6ed251a9f51b136c6811a3c35ac4a641e02ea
Z0rbatrusta <zorbatrusta@gmail.com>
2017-05-28 05:46:38 -0500

    Create colores-bash.sh

---
+++ colores-bash.sh
@@ -0,0 +1,56 @@
+#!/bin/bash 1- Como aprender a programar en bash los diferentes colores y fondos por pantalla. ID 3455468768964535667
+  # 
+  # Este fichero saca por pantalla un montón de códigos de color
+  # para demostrar qué hay disponible. Cada línea es un color con
+  # fondo negro y gris, con el código en medio. Funciona sobre
+  # fondos blancos, negros y verdes
+  #
+  echo "  Sobre gris claro:        Sobre negro:"
+  echo -e "\033[47m\033[1;37m  Blanco        \033[0m\
+   1;37m \
+  \033[40m\033[1;37m  Blanco        \033[0m"
+  echo -e "\033[47m\033[37m  Gris Claro    \033[0m\
+     37m \
+  \033[40m\033[37m  Gris Claro    \033[0m"
+  echo -e "\033[47m\033[1;30m  Gris          \033[0m\
+   1;30m \
+  \033[40m\033[1;30m  Gris          \033[0m"
+  echo -e "\033[47m\033[30m  Negro         \033[0m\
+     30m \
+  \033[40m\033[30m  Negro         \033[0m"
+  echo -e "\033[47m\033[31m  Rojo          \033[0m\
+     31m \#!/bin/bash Como aprender a programar en bash los diferentes col
+  \033[40m\033[31m  Rojo          \033[0m"
+  echo -e "\033[47m\033[1;31m  Rojo Claro    \033[0m\
+   1;31m \
+  \033[40m\033[1;31m  Rojo Claro    \033[0m"
+  echo -e "\033[47m\033[32m  Verde         \033[0m\
+     32m \
+  \033[40m\033[32m  Verde         \033[0m"
+  echo -e "\033[47m\033[1;32m  Verde Claro   \033[0m\
+   1;32m \
+  \033[40m\033[1;32m  Verde Claro   \033[0m"
+  echo -e "\033[47m\033[33m  Marrón        \033[0m\
+     33m \
+  \033[40m\033[33m  Marrón        \033[0m"
+  echo -e "\033[47m\033[1;33m  Amarillo      \033[0m\
+   1;33m \
+  \033[40m\033[1;33m  Amarillo      \033[0m"
+  echo -e "\033[47m\033[34m  Azul          \033[0m\
+     34m \
+  \033[40m\033[34m  Azul          \033[0m"
+  echo -e "\033[47m\033[1;34m  Azul Claro    \033[0m\
+   1;34m \
+  \033[40m\033[1;34m  Azul Claro    \033[0m"
+  echo -e "\033[47m\033[35m  Púrpura       \033[0m\
+     35m \
+  \033[40m\033[35m  Púrpura       \033[0m"
+  echo -e "\033[47m\033[1;35m  Rosa          \033[0m\
+   1;35m \
+  \033[40m\033[1;35m  Rosa          \033[0m"
+  echo -e "\033[47m\033[36m  Cyan          \033[0m\
+     36m \
+  \033[40m\033[36m  Cyan          \033[0m"
+  echo -e "\033[47m\033[1;36m  Cyan Claro    \033[0m\
+   1;36m \
+  \033[40m\033[1;36m  Cyan Claro    \033[0m"
